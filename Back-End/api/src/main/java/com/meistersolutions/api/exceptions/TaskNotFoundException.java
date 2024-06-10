1b842365e4809de9ea7cd006fd5595324a2567ef
Alysson Cauã de Oliveira Thoaldo <alyssuuuuu@outlook.com>
2024-06-09 21:26:45 -0300

    feat:Added a not found message to the TaskNotFounException class

--- Back-End/api/src/main/java/com/meistersolutions/api/exceptions/TaskNotFoundException.java
+++ Back-End/api/src/main/java/com/meistersolutions/api/exceptions/TaskNotFoundException.java
@@ -1,5 +1,9 @@
 package com.meistersolutions.api.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value=HttpStatus.NOT_FOUND,reason="Could not find the specified task")
 public class TaskNotFoundException extends Exception {
     
 }
